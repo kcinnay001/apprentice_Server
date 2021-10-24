32a70d0b1a4bc19a8194b8690f67279740ea5517
Loembetyannick <56350894+YannickLoembet2001@users.noreply.github.com>
2021-10-24 22:51:51 +0100

    second commit

--- index.js
+++ index.js
@@ -17,11 +17,7 @@ const db = mysql.createConnection({
   database : 'heroku_dd707196ed48181'
 }); 
 
-app.use(cors({
-    origin:['https://apprentice-server.herokuapp.com/'],
-    methods:['GET','POST','PUT','DELETE'],
-    credentials:true
-}))
+app.use(cors())
 app.use(cookieParser());
 app.use(express.json())
 app.use(bodyParser.urlencoded({extended:true}))
